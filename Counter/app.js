da3ecebc09eeee33431c83225c057f815aa91d0d
DavidTaapy <DavidTayAP@Hotmail.Com>
2021-07-29 21:24:32 +0800

    Adding color that differs by value for Counter

--- Counter/app.js
+++ Counter/app.js
@@ -15,6 +15,13 @@ btns.forEach(btn => {
         } else if (styles.contains("increase")) {
             count++;
         }
+        if (count > 0) {
+            value.style.color = "green";
+        } else if (count < 0) {
+            value.style.color = "red";
+        } else if (count === 0) {
+            value.style.color = "black";
+        }
         value.textContent = count;
     })
 });
