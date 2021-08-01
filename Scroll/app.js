9a5dc0e712452b5fbc5491494ae41d29b8f4bb13
DavidTaapy <DavidTayAP@Hotmail.Com>
2021-08-01 16:09:34 +0800

    Adding Scroll Template

---
+++ Scroll/app.js
@@ -0,0 +1,15 @@
+// Element.getBoundingClientRect() method returns the size of an element and its position relative to the viewport.
+// PageYOffset is a read - only window property that returns the number of pixels the document has been scrolled vertically.
+// Slice extracts a section of a string without modifying original string
+
+// OffsetTop - A Number, representing the top position of the element, in pixels
+
+// ********** Set Date ************
+
+// ********** Close Links ************
+
+// ********** Fixed Navbar ************
+
+// ********** Smooth Scroll ************
+
+// Select Links
